865e76af44851424e505711971af60bf12f2f6c7
havancuong2003 <havancuong07122003@gmail.com>
2025-10-06 23:13:15 +0700

    fix: fix menu

--- src/menu/staff.js
+++ src/menu/staff.js
@@ -1,11 +1,13 @@
 const staffMenu = [
-  { path: "/dashboard", label: "Dashboard" },
+  { path: "/dashboard", label: "Dashboard", children: [
+    { path: "/dashboard/subdashboard", label: "Subdashboard" },
+  ] },
   { path: "/staff/groups", label: "Nhóm Capstone" },
   // { path: "/reports", label: "Báo cáo & Đánh giá" },
   // { path: "/analytics", label: "Dashboard thống kê" },
   // { path: "/templates", label: "Template báo cáo" },
   // { path: "/tasks", label: "Quản lý Task" },
-  { path: "/settings", label: "Settings" },
+ // { path: "/settings", label: "Settings" },
   { path: "/tracking", label: "Capstone Tracking" },
   { path: "/milestones-management", label: "Milestones Management" },
   { path: "/delivery-management", label: "Delivery Management" },
